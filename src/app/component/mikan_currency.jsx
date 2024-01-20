fe07043728e4778ba7100fec13aa8016fef143e5
℃ (せし) <76618755+NitCelcius@users.noreply.github.com>
2024-01-21 00:24:52 +0900

    Add currency display
    通貨の状況を表示できるようにしました

---
+++ src/app/component/mikan_currency.jsx
@@ -0,0 +1,27 @@
+export default function MikanCurrency(props) {
+    return (
+            <div className=
+                     "
+                      flex
+                      flex-row
+                      justify-center
+                      self-center
+                      p-1
+                      border-4
+                      bg-amber-200
+                      border-amber-200
+                      box-border
+                      rounded-2xl
+                      drop-shadow-md">
+                <span className="text-md">
+                    みかん通貨
+                </span>
+                <p className="pl-2
+                text-2xl
+                ">
+                    <a>{props.currencyAmount}</a>
+                    <span className="text-sm p-0.5">mp</span>
+                </p>
+            </div>
+    )
+}
