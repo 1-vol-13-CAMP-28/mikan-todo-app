dbaab39a57043edf39b74412ed88e380309f8dd1
℃ (せし) <76618755+NitCelcius@users.noreply.github.com>
2024-01-21 15:07:09 +0900

    Fix PWA work

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withPWA = require('next-pwa')({
     skipWaiting: true,
     scope: "/",
     sw: "sw.js",
-    disable: process.env.NODE_ENV === 'development', // 開発中はPWA無効
+//    disable: process.env.NODE_ENV === 'development', // 開発中はPWA無効
     runtimeCaching,
     buildExcludes: [/middleware-manifest.json$/],
     publicExcludes: []
